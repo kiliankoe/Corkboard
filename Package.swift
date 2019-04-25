06cebc246f001121d1f69e816423cb703018610c
Kilian Koeltzsch <me@kilian.io>
2019-04-25 16:39:23 +0200

    add other platforms to manifest

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "Corkboard",
     platforms: [
-        .macOS(.v10_12)
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .watchOS(.v3),
+        .tvOS(.v10)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
